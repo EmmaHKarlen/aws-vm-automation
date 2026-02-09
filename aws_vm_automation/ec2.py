75d0d2fbe6c2ac862d7df55bcdb9af906e30d1b0
EmmaHKarlen <emmahkarlen@gmail.com>
2026-02-09 16:54:06 +0200

    feat: create and terminate EC2 instance via boto3

--- aws_vm_automation/ec2.py
+++ aws_vm_automation/ec2.py
@@ -1,4 +1,9 @@
+import time
+from typing import Optional
 import boto3
+
+UBUNTU_OWNER_CANONICAL = "099720109477"
+UBUNTU_2204_NAME_PATTERN = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
 
 def find_latest_ubuntu_ami(region: str) -> str:
     ec2 = boto3.client("ec2", region_name=region)
@@ -22,3 +27,51 @@ def find_latest_ubuntu_ami(region: str) -> str:
 
     latest_image = images[-1]
     return latest_image["ImageId"]
+
+
+def create_instance(
+    region: str,
+    key_name: str,
+    instance_type: str = "t2.micro",
+) -> str:
+    ec2 = boto3.client("ec2", region_name=region)
+    ami_id = find_latest_ubuntu_ami(region)
+
+    resp = ec2.run_instances(
+        ImageId=ami_id,
+        InstanceType=instance_type,
+        KeyName=key_name,
+        MinCount=1,
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                "ResourceType": "instance",
+                "Tags": [
+                    {"Key": "Project", "Value": "aws-vm-automation"},
+                ],
+            }
+        ],
+    )
+
+    instance_id = resp["Instances"][0]["InstanceId"]
+    return instance_id
+
+
+def wait_until_running(region: str, instance_id: str) -> None:
+    ec2 = boto3.client("ec2", region_name=region)
+    waiter = ec2.get_waiter("instance_running")
+    waiter.wait(InstanceIds=[instance_id])
+
+
+def get_public_ip(region: str, instance_id: str) -> Optional[str]:
+    ec2 = boto3.client("ec2", region_name=region)
+    resp = ec2.describe_instances(InstanceIds=[instance_id])
+    inst = resp["Reservations"][0]["Instances"][0]
+    return inst.get("PublicIpAddress")
+
+
+def terminate_instance(region: str, instance_id: str) -> None:
+    ec2 = boto3.client("ec2", region_name=region)
+    ec2.terminate_instances(InstanceIds=[instance_id])
+    waiter = ec2.get_waiter("instance_terminated")
+    waiter.wait(InstanceIds=[instance_id])
--- aws_vm_automation/main.py
+++ aws_vm_automation/main.py
@@ -1,5 +1,33 @@
-from aws_vm_automation.ec2 import find_latest_ubuntu_ami
+from aws_vm_automation.ec2 import (
+    find_latest_ubuntu_ami,
+    create_instance,
+    get_public_ip,
+    terminate_instance,
+    wait_until_running,
+)
+
+REGION = "eu-central-1"
+KEY_NAME = "jr-key"
+
+
+def main():
+    ami = find_latest_ubuntu_ami(REGION)
+    print("Latest Ubuntu AMI:", ami)
+
+    print("Creating EC2 instance...")
+    instance_id = create_instance(region=REGION, key_name=KEY_NAME)
+    print("InstanceId:", instance_id)
+
+    print("Waiting until running...")
+    wait_until_running(region=REGION, instance_id=instance_id)
+
+    ip = get_public_ip(region=REGION, instance_id=instance_id)
+    print("Public IP:", ip)
+
+    print("Terminating instance...")
+    terminate_instance(region=REGION, instance_id=instance_id)
+    print("Done.")
+
 
 if __name__ == "__main__":
-    ami = find_latest_ubuntu_ami("eu-central-1")
-    print("Latest Ubuntu AMI:", ami)
+    main()
